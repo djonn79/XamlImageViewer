9950ea7b056ffaebe009d7d1de690e36e4716ad1
Alx <bender.rdgz.2996@gmail.com>
2020-05-12 14:05:25 +1000

    Добавил ViewModelBase

---
+++ XamlImageViewer/ViewModelBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace XamlImageViewer
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {      
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            try
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err?.InnerException.Message ?? err.Message, err.Message, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public bool EnableEditMode { get; set; } = true;
+    }
+}
